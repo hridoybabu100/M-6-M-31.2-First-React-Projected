98f8d71f506a27a6f8484b052af74648c3bd9c76
Hridoy Hasan Babu <hridoyakanda100@gmail.com>
2026-03-13 17:10:22 +0600

    todo jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 
 import './App.css'
+import ToDo from './todo';
+// import system connected
+// 1. import
+// 2. Name 
+// 3. from
+// 4. ''
 
 function App() {
   
 
+  const time = 50;
+
   return (
     <>
-       <h1>This is React Newspeed</h1>
-       <Person></Person>
-       <Student name="Hridoy Akanda" dep="Math"></Student>
-       <Student name="Jecika Jeci" dep="physics"></Student>
-       <Student name="Monika shill" dep="Higher math"></Student>
-      <Devoloper name="Rayhan" tech="Phthon"></Devoloper>
-      <Devoloper name="Yeasin ahmed" tech="JavaScript"></Devoloper>
-      <Details name="Hridoy Akanda" age="22" ocupation="student"></Details>
-      <Details name="Esrat Muslim Esha" age="16" ocupation="student"></Details>
-      <Details name="Hridita Akanda" age="5" ocupation="student"></Details>
+      <h1>React Core Concept</h1>
+      {/* //  <h1>This is React Newspeed</h1>
+      //  <Person></Person>
+      //  <Student name="Hridoy Akanda" dep="Math"></Student>
+      //  <Student name="Jecika Jeci" dep="physics"></Student>
+      //  <Student name="Monika shill" dep="Higher math"></Student>
+      // <Devoloper name="Rayhan" tech="Phthon"></Devoloper>
+      // <Devoloper name="Yeasin ahmed" tech="JavaScript"></Devoloper>
+      // <Details name="Hridoy Akanda" age="22" ocupation="student"></Details>
+      // <Details name="Esrat Muslim Esha" age="16" ocupation="student"></Details>
+      // <Details name="Hridita Akanda" age="5" ocupation="student"></Details> */}
+
+      {/* // Todo function */}
+
+      <ToDo task="Learn react" isDone={true} time={time}></ToDo>
+      <ToDo task="Learn java" isDone={false}></ToDo>
+      <ToDo task="Take a shawer" isDone={true}></ToDo>
+
+      
     
     </>
   )
---
+++ src/todo.jsx
@@ -0,0 +1,63 @@
+// New Function curiculam vite
+
+// export default function ToDo ({name, age}){
+//     return(
+//         <div className="student">
+//             <h1>This is name of heading</h1>
+//             <p>Name : {name}</p>
+//             <p>Technology : {age}</p>
+//         </div>
+//     );
+// };
+
+// export default function ToDo ({task, isDone}){
+//     return(
+        
+//             <li>tasks : {task} </li>
+        
+//     )
+// }
+
+
+// Condition Alarming
+
+// export default function ToDo ({task, isDone}) {
+//    if (isDone){
+//     return <li>task : {task}</li>
+    
+//    }
+//    else{
+//     <li>task : </li>
+//    }
+// } 
+// export default function ToDo ({task, isDone, time = 0}) {
+//    if (isDone === true){
+//     return <li>task : {task} {time}</li>
+    
+//    }
+//    else{
+//     return <li>Tobe be Pending : {task}</li>
+//    }
+// } 
+
+// condition rendering
+// Condition ? true : false
+
+// export default function ToDo ({task, isDone, time = 0}){
+//     return isDone ? <li>Done : {task} {isDone} Duration : {time} </li> : 
+//     <li>Not Done : {task}</li>
+// }
+
+
+// Ehhn Ami sob sotto gula ke dekhate chai && . done jodi sotto hoi tahole vitore jaw
+// export default function ToDo ({task, isDone, time = 0}){
+//     return isDone && <li>Done : {task}</li>
+// }
+
+
+
+// Ehhn Ami sob mittha gula ke dekhate chai || . done jodi sotto hoi tahole vitore jaw
+export default function ToDo ({task, isDone, time = 0}){
+    return isDone || <li> Pending : {task}</li>
+}
+
